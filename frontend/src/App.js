e0c9180cf8fa735fbb7166ff9a4aa64376062123
rxshellg <rashellguerrero123@gmail.com>
2025-02-20 17:06:01 -0500

    Started work on homepage and changed file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage.jsx';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;

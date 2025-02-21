df90ee5873455f5a48337d8324e1ddb1770fa3bd
rxshellg <rashellguerrero123@gmail.com>
2025-02-20 19:29:10 -0500

    Added number of the day- logic tbm

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import NavBar from '../components/NavBar'
 import JokeOfTheDayCard from '../components/JokeOfTheDayCard';
+import NumberOfTheDayCard from '../components/NumberOfTheDayCard';
 
 const Homepage = () => {
     return (
         <>
             <NavBar />
             <JokeOfTheDayCard />
+            <NumberOfTheDayCard />
         </>
     )
 }
